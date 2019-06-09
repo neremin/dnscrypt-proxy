21d34048f1418309473b87e3c9175c1a3930d7da
N1c0 <neremin@users.noreply.github.com>
2019-06-09 20:52:42 +0300

    Fallback resolver as connectivity test
    Sometimes DNS is not available during startup, but the dnscrypt-proxy uses fallback resolver in such situation. Hence, pinging fallback resolver IP is enough for connectivity test. Besides it's done automatically in the latest 2.0.25 dnscrypt version as well.

--- common/service.sh
+++ common/service.sh
@@ -7,7 +7,7 @@
 MODDIR=${0%/*}
 
 for i in 1 2 3 4 5 6 7 8 9 10 11 12; do
-	ping -c 1 download.dnscrypt.info
+	ping -c 1 9.9.9.9
 	if [[ $? == 0 ]];
 	then
 		$MODDIR/system/bin/dnscrypt-proxy -config $MODDIR/system/etc/dnscrypt-proxy/dnscrypt-proxy.toml &
